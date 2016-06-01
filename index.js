9b33ff60d08aafaadded0ea77fe20f3fe164b2f1
fruitet <rfruitet@gmail.com>
2016-06-01 17:20:20 +0200

    ok

--- index.js
+++ index.js
@@ -3,7 +3,24 @@ var api = express();
 var DEFAULT_PORT = 3000;
 
 api.get('/', function(req, res, next) {
-	res.send('Hello World');
+	res.send([]);
+})
+
+api.get('/contacts', function(req, res, next) {
+	res.send();
+})
+
+api.post('/contacts/:name', function(req, res, next) {
+	if (req.params)
+	res.send();
+})
+
+api.put('/contacts/:name/:new', function(req, res, next) {
+	res.send();
+})
+
+api.delete('/contacts/:name', function(req, res, next) {
+	res.send();
 })
 
 var port = process.env.PORT || DEFAULT_PORT;
